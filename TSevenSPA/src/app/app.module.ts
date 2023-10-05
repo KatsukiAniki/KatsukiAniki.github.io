9962198c8bd1d89a6874c8fdf9d1e07d7b0c7e06
Cengiz <cengiz.monteforte@plan-b-gmbh.com>
2023-10-05 15:57:16 +0200

    added material

--- TSevenSPA/src/app/app.module.ts
+++ TSevenSPA/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { EventsComponent } from './components/events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeamsComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
