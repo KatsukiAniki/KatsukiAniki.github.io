318dded6879d3a6a9511d29dd7612d5f3b5e77d3
Cengiz <cengiz.monteforte@plan-b-gmbh.com>
2023-10-24 16:55:51 +0200

    setup frontend and home view

--- TSevenSPA/src/app/app-routing.module.ts
+++ TSevenSPA/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { EventsComponent } from './components/events/events.component';
+import { TeamsComponent } from './components/teams/teams.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'/home', pathMatch: 'full'},
-  {path:'home', component: HomeComponent}
+  {path:'home', component: HomeComponent},
+  {path:'events', component: EventsComponent},
+  {path:'teams', component: TeamsComponent},
+
 ];
 
 @NgModule({
--- TSevenSPA/src/app/app.module.ts
+++ TSevenSPA/src/app/app.module.ts
@@ -2,23 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TeamsComponent } from './components/teams/teams.component';
 import { EventsComponent } from './components/events/events.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { LayoutComponent } from './components/layout/layout.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
     HomeComponent,
     TeamsComponent,
-    EventsComponent
+    EventsComponent,
+    NavbarComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    MatTabsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
