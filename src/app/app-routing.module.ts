1b6525676c174d2fbab872fb5c8cf11d8771d97f
KatsukiAniki <montecgi@gmail.com>
2024-01-13 20:27:20 +0100

    builded application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { EventsComponent } from './components/events/events.component';
 import { TeamsComponent } from './components/teams/teams.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const routes: Routes = [
   {path:'', redirectTo:'/home', pathMatch: 'full'},
   {path:'home', component: HomeComponent},
   {path:'events', component: EventsComponent},
   {path:'teams', component: TeamsComponent},
+  { path: '**', redirectTo: '' }, // Wildcard route for navigation fallback
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
 })
 export class AppRoutingModule { }
